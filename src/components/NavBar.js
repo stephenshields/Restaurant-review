7f7922114864bb22eb60b76f6ff6720df5280c24
stephen shields <stephen.shields@live.ie>
2020-05-21 17:07:33 +0100

    CSS changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,38 +1,38 @@
 import React from 'react'
-import {Navbar} from 'react-bootstrap'
+import { Navbar, Col} from 'react-bootstrap'
 import Filter from '../components/Filter'
 
 function NavBar(props) {
-	return	(
-		<Navbar 
+    return (
+        <Navbar 
 		variant="dark"
 		style={{
 			backgroundColor: '#21242b',
-			padding: '2px'
 		}}> 
-    <Navbar.Brand style={{paddingLeft: '5px'}} href="#home">Lets Eat</Navbar.Brand>
-     <Navbar.Collapse className="justify-content-end mr-3" >
-     <Navbar.Text className="pr-3">
-     Filter From
-     </Navbar.Text>
-    <Filter
-    key="1"
-    keyNum="1"
-    setFilter={props.setFilter}
-    >
-    </Filter>
-    <Navbar.Text className="pr-3 pl-3">
-     To
-     </Navbar.Text>
-    <Filter
-    key="2"
-    keyNum="2"
-    setFilter={props.setFilter}>
-    </Filter>
-     </Navbar.Collapse>
+		<Col xs={3} >
+	    	<Navbar.Brand href="#home">Lets Eat</Navbar.Brand>
+	    </Col>
+	    <Col xs={9} >
+		     <Navbar.Collapse className="justify-content-end" >
+			    <Filter
+			    key="1"
+			    keyNum="1"
+			    setFilter={props.setFilter}
+			    >
+			    </Filter>
+			    <Navbar.Text className="pr-2 pl-2">
+			     -
+			     </Navbar.Text>
+			    <Filter
+			    key="2"
+			    keyNum="2"
+			    setFilter={props.setFilter}>
+			    </Filter>
+		     </Navbar.Collapse>
+	     </Col>
   </Navbar>
 
-	)
+    )
 }
 
 export default NavBar;
